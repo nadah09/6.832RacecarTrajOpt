811dcd31ee0d957576f67612a392af4b92cc8293
nada <nhussein@mit.edu>
2021-04-29 23:48:28 -0400

    fixed dynamics utils

--- dynamics_utils.py
+++ dynamics_utils.py
@@ -25,7 +25,7 @@ class Dynamics:
 
 	def discrete_dynamics(x, u):
 	    dt = 0.1
-	    x_next = x + dt * car_continuous_dynamics(x, u)
+	    x_next = x + dt * self.car_continuous_dynamics(x, u)
 	    return x_next
 
 	def rollout(x0, u_trj):
@@ -33,5 +33,5 @@ class Dynamics:
 	    x_trj[0] = x0
 	    # TODO: Define the rollout here and return the state trajectory x_trj: [N, number of states]
 	    for i in range(1, u_trj.shape[0] + 1):
-	      x_trj[i] = discrete_dynamics(x_trj[i - 1], u_trj[i - 1])
+	      x_trj[i] = self.discrete_dynamics(x_trj[i - 1], u_trj[i - 1])
 	    return x_trj
