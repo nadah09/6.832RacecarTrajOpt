091dab170647f14826bc77aab5d31dfce6282922
nada <nhussein@mit.edu>
2021-04-30 13:12:06 -0400

    changed utils

--- cost_utils.py
+++ cost_utils.py
@@ -2,29 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-class Cost():
-	def __init__(self):
-		self.r = 2.0
-		self.v_target = 2.0
-		self.eps = 1e-6
 
-	v_target = 2.0
-	eps = 1e-6 # The derivative of sqrt(x) at x=0 is undefined. Avoid by subtle smoothing
-	def cost_stage(x, u):
-	    m = sym if x.dtype == object else np # Check type for autodiff
-	    c_circle = (m.sqrt(x[0]**2 + x[1]**2 + self.eps) - self.r)**2
-	    c_speed = (x[3]-self.v_target)**2
-	    c_control= (u[0]**2 + u[1]**2)*0.1
-	    return c_circle + c_speed + c_control
+r = 2.0
+v_target = 2.0
+eps = 1e-6 # The derivative of sqrt(x) at x=0 is undefined. Avoid by subtle smoothing
+def cost_stage(x, u):
+    m = sym if x.dtype == object else np # Check type for autodiff
+    c_circle = (m.sqrt(x[0]**2 + x[1]**2 + eps) - r)**2
+    c_speed = (x[3]-v_target)**2
+    c_control= (u[0]**2 + u[1]**2)*0.1
+    return c_circle + c_speed + c_control
 
-	def cost_final(x):
-	    m = sym if x.dtype == object else np # Check type for autodiff
-	    c_circle = (m.sqrt(x[0]**2 + x[1]**2 + self.eps) - self.r)**2
-	    c_speed = (x[3]-self.v_target)**2
-	    return c_circle + c_speed
+def cost_final(x):
+    m = sym if x.dtype == object else np # Check type for autodiff
+    c_circle = (m.sqrt(x[0]**2 + x[1]**2 + eps) - r)**2
+    c_speed = (x[3]-v_target)**2
+    return c_circle + c_speed
 
-	def cost_trj(x_trj, u_trj):
-	    total = 0.0
-	    total = sum([cost_stage(x_trj[i, :], u_trj[i, :]) for i in range(x_trj.shape[0]-1)])
-	    total += cost_final(x_trj[-1, :])
-	    return total
+def cost_trj(x_trj, u_trj):
+    total = 0.0
+    total = sum([cost_stage(x_trj[i, :], u_trj[i, :]) for i in range(x_trj.shape[0]-1)])
+    total += cost_final(x_trj[-1, :])
+    return total
--- dynamics_utils.py
+++ dynamics_utils.py
@@ -3,35 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-class Dynamics:
-	def __init__(self):
-		self.n_x = 5
-		self.n_u = 2
-	def car_continuous_dynamics(x, u):
-	    # x = [x position, y position, heading, speed, steering angle] 
-	    # u = [acceleration, steering velocity]
-	    m = sym if x.dtype == object else np # Check type for autodiff
-	    heading = x[2]
-	    v = x[3]
-	    steer = x[4]
-	    x_d = np.array([
-	        v*m.cos(heading),
-	        v*m.sin(heading),
-	        v*m.tan(steer),
-	        u[0],
-	        u[1]        
-	    ])
-	    return x_d
 
-	def discrete_dynamics(x, u):
-	    dt = 0.1
-	    x_next = x + dt * self.car_continuous_dynamics(x, u)
-	    return x_next
+def car_continuous_dynamics(x, u):
+    # x = [x position, y position, heading, speed, steering angle] 
+    # u = [acceleration, steering velocity]
+    m = sym if x.dtype == object else np # Check type for autodiff
+    heading = x[2]
+    v = x[3]
+    steer = x[4]
+    x_d = np.array([
+        v*m.cos(heading),
+        v*m.sin(heading),
+        v*m.tan(steer),
+        u[0],
+        u[1]        
+    ])
+    return x_d
 
-	def rollout(x0, u_trj):
-	    x_trj = np.zeros((u_trj.shape[0]+1, x0.shape[0]))
-	    x_trj[0] = x0
-	    # TODO: Define the rollout here and return the state trajectory x_trj: [N, number of states]
-	    for i in range(1, u_trj.shape[0] + 1):
-	      x_trj[i] = self.discrete_dynamics(x_trj[i - 1], u_trj[i - 1])
-	    return x_trj
+def discrete_dynamics(x, u):
+    dt = 0.1
+    x_next = x + dt * self.car_continuous_dynamics(x, u)
+    return x_next
+
+def rollout(x0, u_trj):
+    x_trj = np.zeros((u_trj.shape[0]+1, x0.shape[0]))
+    x_trj[0] = x0
+    # TODO: Define the rollout here and return the state trajectory x_trj: [N, number of states]
+    for i in range(1, u_trj.shape[0] + 1):
+      x_trj[i] = self.discrete_dynamics(x_trj[i - 1], u_trj[i - 1])
+    return x_trj
